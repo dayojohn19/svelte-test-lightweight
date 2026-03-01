93202af0c1a064b6b7e517303cbde357e805f5a8
dayojohn19 <dayo_john16@yahoo.com>
2026-03-01 12:22:28 +0800

    working

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  base: '/svelte-test-lightweight/',
   plugins: [svelte()]
 });
